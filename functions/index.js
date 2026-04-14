59dc32a80051459b86759bf6a610495a42d8c193
deltasprey <deltasprey@gmail.com>
2026-04-14 21:49:27 +1000

    Added loading from firestore

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
-const functions = require("firebase-functions");
+const { onRequest } = require("firebase-functions/https");
+const admin = require("firebase-admin");
+//const { getFunctions } = require("firebase-admin/functions");
 
-exports.token = functions.https.onRequest({ secrets: ["DISCORD_CLIENT_SECRET"] }, async (req, res) => {
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.token = onRequest({ secrets: ["DISCORD_CLIENT_SECRET"] }, async (req, res) => {
     try {
         const { code } = req.body;
         if (!code) {
@@ -33,18 +38,61 @@ exports.token = functions.https.onRequest({ secrets: ["DISCORD_CLIENT_SECRET"] }
     }
 });
 
-exports.send_message = functions.https.onRequest({ secrets: ["DISCORD_TOKEN"] }, async (req, res) => {
-    const { content, channelId } = req.body;
-    if (!content || !channelId) {
-        return res.status(400).json({ error: "Missing code" });
-    }
+exports.send_message = onRequest({ secrets: ["DISCORD_TOKEN"] }, async (req, res) => {
+    try {
+        const { content, channelId } = req.body;
+        if (!content || !channelId) {
+            return res.status(400).json({ error: "Missing code" });
+        }
 
-    const response = await fetch(`https://discord.com/api/v10/channels/${channelId}/messages`, {
-        method: "POST",
-        headers: { "Authorization": `Bot ${process.env.DISCORD_TOKEN}`, "Content-Type": "application/json" },
-        body: JSON.stringify({ content: content })
-    });
-    const messageRes = await response.json();
-    if (!response.ok) { return res.status(response.status).json(messageRes); }
-    return res.json(messageRes);
+        const response = await fetch(`https://discord.com/api/v10/channels/${channelId}/messages`, {
+            method: "POST",
+            headers: { "Authorization": `Bot ${process.env.DISCORD_TOKEN}`, "Content-Type": "application/json" },
+            body: JSON.stringify({ content: content })
+        });
+        const messageRes = await response.json();
+        if (!response.ok) { return res.status(response.status).json(messageRes); }
+        return res.json(messageRes);
+    } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+exports.get_initial_data = onRequest(async (req, res) => {
+    try {
+        const { guild_id } = req.body;
+        if (!guild_id) {
+            return res.status(400).json({ error: "Missing guild_id" });
+        }
+
+        const docRef = db.collection("Guild_Buttons").doc(guild_id);
+        const docSnap = await docRef.get();
+        if (docSnap.exists) { return res.json(docSnap.data()); }
+
+        const newData = { buttons: [] };
+        await docRef.set(newData);
+        return res.json(newData);
+    } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+exports.send_future_message = onRequest(async (req, res) => {
+    try {
+        const { channelId } = req.body;
+        if (!channelId) {
+            return res.status(400).json({ error: "Missing code" });
+        }
+
+        // await getFunctions().taskQueue("processTask").enqueue(
+        //     { content: "Example scheduled message", channelId: channelId },
+        //     { scheduleTime: new Date(Date.now() + 30 * 1000) } // 30 seconds
+        // );
+        res.json({ message: "Task scheduled!" });
+    } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
 });
--- main.js
+++ main.js
@@ -1,36 +1,65 @@
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
+// Setup scheduled message button
+document.getElementById("future-btn").onclick = async () => {
+  await fetch("send_future_message", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ channelId: "1490576898177236992" })
+  });
+};
+
+// Connect to Discord
 const status = document.getElementById("status");
-const btn = document.getElementById("btn");
-const count = document.getElementById("count");
 status.innerHTML = "Connecting to Discord...";
 
 const sdk = new DiscordSDK("1491007176511193128");
 await sdk.ready();
 status.innerHTML = "Fetching User ID...";
 
+// Authorise
 const { code } = await sdk.commands.authorize({
   client_id: "1491007176511193128",
   response_type: "code",
   scope: ["identify"],
   prompt: "none"
 });
-const res = await fetch("token", {
+
+// Get User ID
+const userResponse = await fetch("token", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ code }),
+  body: JSON.stringify({ code: code })
 });
-if (res.ok) {
-  const { user_id } = await res.json();
-  let clickCount = 0;
-  status.innerHTML = "Ready";
-  btn.disabled = false;
-  btn.onclick = async () => {
-    await fetch("activity_button_click", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ content: `Button clicked by <@${user_id}>`, channelId: "1490576898177236992" })
-    });
-    count.innerHTML = ++clickCount;
-  };
+if (userResponse.ok) {
+  const { user_id } = await userResponse.json();
+  status.innerHTML = `Retrieving Initial Data for ${sdk.guildId}...`;
+
+  // Retrieve Initial Data
+  const initResponse = await fetch("get_initial_data", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ guild_id: sdk.guildId })
+  });
+  if (initResponse.ok) {
+    // Add buttons
+    const { buttons } = await initResponse.json();
+    let containerHtml = "";
+    for (let i = 0; i < buttons.length; i++) { containerHtml += `<button id="btn${i}">${buttons[i]}</button>` }
+
+    document.getElementById("btn-container").innerHTML = containerHtml;
+
+    // Add button click handling
+    for (let i = 0; i < buttons.length; i++) {
+      document.getElementById(`btn${i}`).onclick = async () => {
+        await fetch("activity_button_click", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ content: `Button ${i} clicked by <@${user_id}>`, channelId: "1490576898177236992" })
+        });
+      };
+    }
+
+    status.innerHTML = "Ready";
+  } else { status.innerHTML = "Initial Data Retrieval Failed"; }
 } else { status.innerHTML = "Fetch Failed"; }
